3e8d9a7a56ca9d4a7954948330d49c7242a6f3ea
Victor Yao <55715390+VictorVy@users.noreply.github.com>
2021-12-03 18:15:43 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,32 +7,32 @@ public class Main
     {
         System.out.println("Sieve of Eratosthenes: " + getPrimes(100));
         System.out.println("Goldbach Conjecture: " + getPrimeAddends(100));
-        System.out.println("Adding BigInts: " + add(new ArrayList<>(Arrays.asList(5, 5, 5)), new ArrayList<>(Arrays.asList(5, 5, 5))));
+        System.out.println("Adding BigInts: " + add(new ArrayList<>(Arrays.asList(5, 5, 5)), new ArrayList<>(Arrays.asList(5, 5, 5, 5))));
     }
-    
+
     public static ArrayList<Integer> getPrimes(int n)
     {
         ArrayList<Integer> primes = new ArrayList<>();
         primes.add(2);
-        
+
         for(int i = 3; i < n; i += 2)
         {
             boolean b = true;
-            
+
             for(int j = 0; j < primes.size() - 1 && b; j++)
-                if(i % primes.get(j) == 0) b = false;
-                
+                b = (i % primes.get(j) != 0);
+
             if(b) primes.add(i);
         }
-        
+
         return primes;
     }
-    
+
     public static ArrayList<Integer> getPrimeAddends(int n)
     {
         ArrayList<Integer> primes = getPrimes(n), addends = new ArrayList<>();
-        
-        for(int i = 0; i < primes.size() && addends.size() < 2; i++)
+
+        for(int i = 0; i < primes.size() && addends.isEmpty(); i++)
         {
             if(primes.contains(n - primes.get(i)))
             {
@@ -40,40 +40,40 @@ public class Main
                 addends.add(n - primes.get(i));
             }
         }
-        
+
         return addends;
     }
-    
+
     public static ArrayList<Integer> add(ArrayList<Integer> a, ArrayList<Integer> b)
     {
         ArrayList<Integer> sum = new ArrayList<>();
-        
+
         if(a.size() < b.size())
         {
             ArrayList<Integer> tmp = a;
             a = b;
             b = tmp;
         }
-        
+
         int carry = 0;
-        
+
         for(int i = 0; i < a.size(); i++)
         {
-            int aD = a.get(a.size() - 1 - i);
+            int aD = a.get(a.size() - i - 1);
 
             if(i < b.size())
             {
-                int bD = b.get(b.size() - 1 - i);
+                int bD = b.get(b.size() - i - 1);
                 sum.add(0, (aD + bD + carry) % 10);
-                carry = (aD + bD) / 10;
+                carry = (aD + bD + carry) / 10;
             }
             else
             {
-                if(i == b.size() + 1) carry = 0;
-                sum.add(0, aD + carry);
+                sum.add(0, (aD + carry) % 10);
+                carry = (aD + carry) / 10;
             }
         }
-        
+
         if(carry == 1) sum.add(0, 1);
         return sum;
     }
